943fb961a0f104e4e67e81288c13585675cf0225
Mohammed Mubarak <44917891+MDmubarak786@users.noreply.github.com>
2021-04-03 17:54:01 +0530

    good

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import { useState } from "react";
 import "./styles.css";
 
+function Button1(props){
+  return(
+  <div>
+  <button onClick={() => props.adding_value1(props.incrementvalue1) }>100+</button>
+  <span>{props.newvalue1}</span>
+  </div>
+  );
+}
+function Button2(props){
+  return(
+  <div>
+  <button onClick={() => props.adding_value2(props.incrementvalue2)}>5+</button>
+  <span>{props.newvalue2}</span>
+  </div>
+  );
+}
+
 export default function App() {
-  const [newnum, oldnum] = useState(0);
+  const [newnum2, oldnum2] = useState(0);
+  const [newnum1, oldnum1] = useState(0);
+  const adding = (increment) => oldnum1(newnum1+increment);
+  const adding2 = (increment2) => oldnum2(newnum2+increment2);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>{newnum}</h2>
-      <h2>{oldnum}</h2>
-      <button onClick={() => oldnum(newnum + 1)}>click</button>
-      <h2>{oldnum}</h2>
+     <Button1 adding_value1={adding} newvalue1={newnum1} incrementvalue1={100}/>
+     <Button2 adding_value2={adding2} newvalue2={newnum2} incrementvalue2={5}/>
     </div>
   );
 }
